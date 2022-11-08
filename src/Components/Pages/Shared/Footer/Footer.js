0076dec0cb57dd04fc0489f73e80d6f0e0d21397
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-08 12:05:31 +0600

    footer section design done

--- src/Components/Pages/Shared/Footer/Footer.js
+++ src/Components/Pages/Shared/Footer/Footer.js
@@ -1,9 +1,35 @@
 import React from "react";
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaYoutubeSquare,
+  FaGooglePlusSquare,
+  FaLinkedin,
+} from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>footer</h1>
+    <div className="bg-gray-600 py-6">
+      <div className="flex justify-center gap-6 mb-4">
+        <a href="https://www.facebook.com/">
+          <FaFacebookSquare className="text-white text-2xl" />
+        </a>
+        <a href="https://twitter.com/">
+          <FaTwitterSquare className="text-white text-2xl" />
+        </a>
+        <a href="https://www.youtube.com/">
+          <FaYoutubeSquare className="text-white text-2xl" />
+        </a>
+        <a href="https://www.google.com/">
+          <FaGooglePlusSquare className="text-white text-2xl" />
+        </a>
+        <a href="https://www.linkedin.com/">
+          <FaLinkedin className="text-white text-2xl" />
+        </a>
+      </div>
+      <p className="text-center font-medium text-white">
+        Copyright @2022 || All rights are reserved by the Shahi Kitchen.
+      </p>
     </div>
   );
 };
