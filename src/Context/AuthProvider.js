c3e8199bbd84445bec9e20f9756ca689f33c4e5f
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-08 16:02:21 +0600

    login user with email and password system added

--- src/Context/AuthProvider.js
+++ src/Context/AuthProvider.js
@@ -1,5 +1,9 @@
 import React, { createContext, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext();
@@ -15,9 +19,16 @@ const AuthProvider = ({ children }) => {
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
+  //user login with email and password
+  const userLoginWithEmailAndPassword = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
   const authInfo = {
     user,
     creatUser,
+    userLoginWithEmailAndPassword,
     loading,
     setLoading,
     setUser,
