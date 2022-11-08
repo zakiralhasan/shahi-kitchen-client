10c4928b73828d8ec856a51d035d8065f3ea6218
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-08 15:50:06 +0600

    create user with email and password system added

--- src/Components/Pages/Register/Register.js
+++ src/Components/Pages/Register/Register.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../Context/AuthProvider";
 
 const Register = () => {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const { creatUser } = useContext(AuthContext);
+
+  const handleForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.name.value;
+    const imgURL = form.imgURL.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, imgURL, email, password);
+
+    creatUser(email, password)
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        form.reset();
+        setErrorMessage("");
+      })
+      .catch((error) => {
+        const errorMsg = error.message;
+        setErrorMessage(errorMsg);
+        form.reset();
+      });
+  };
   return (
     <div>
-      {" "}
-      <form>
+      <form onSubmit={handleForm}>
         <div className=" bg-base-200">
           <div className="hero-content flex-col ">
             <div className="card rounded-md flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
@@ -60,6 +85,8 @@ const Register = () => {
                       Already have an account?
                     </Link>
                   </label>
+                  {/* show error message at login time */}
+                  <p className="text-xs text-red-500">{errorMessage}</p>
                 </div>
                 <div className="form-control mt-6">
                   <button className="btn bg-blue-400 border-none">
