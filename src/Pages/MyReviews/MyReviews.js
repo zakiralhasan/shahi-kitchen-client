6329461287020f84d9230ed52c2e4797e64b8f3b
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-09 23:20:45 +0600

    single review delete system added at my reviews page

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -2,16 +2,50 @@ import React from "react";
 import { useContext } from "react";
 import { useEffect } from "react";
 import { useState } from "react";
+import swal from "sweetalert";
 import { AuthContext } from "../../Context/AuthProvider";
 
 const MyReviews = () => {
   const { user } = useContext(AuthContext);
   const [myReviews, setMyReviews] = useState([]);
+
   useEffect(() => {
     fetch(`http://localhost:5000/reviews?email=${user?.email}`)
       .then((res) => res.json())
       .then((data) => setMyReviews(data));
   }, [user?.email]);
+
+  //used for delete single review from reviews collection on mongodb
+  const handleDelete = (id) => {
+    swal({
+      title: "Are you sure?",
+      text: "Once deleted, you will not be able to recover this review!",
+      icon: "warning",
+      buttons: true,
+      dangerMode: true,
+    }).then((willDelete) => {
+      if (willDelete) {
+        fetch(`http://localhost:5000/reviews/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              swal("Poof! Your review has been deleted!", {
+                icon: "success",
+              });
+              const remainingReviews = myReviews.filter(
+                (reviews) => reviews._id !== id
+              );
+              setMyReviews(remainingReviews);
+            }
+          });
+      } else {
+        swal("Your review is safe!");
+      }
+    });
+  };
   return (
     <div>
       <h1>my reviews</h1>
@@ -32,7 +66,10 @@ const MyReviews = () => {
                   <button className="text-blue-400 font-medium mr-3 ">
                     <small>Update</small>
                   </button>
-                  <button className="text-red-400 font-medium mr-3 ">
+                  <button
+                    onClick={() => handleDelete(review._id)}
+                    className="text-red-400 font-medium mr-3 "
+                  >
                     <small>Delete</small>
                   </button>
                 </div>
