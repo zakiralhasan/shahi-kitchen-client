3fe87183c2c96442c7b0e6ca058e48aa59084d7f
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-10 00:19:14 +0600

    add single service system has been added

--- src/Pages/AddServices/AddServices.js
+++ src/Pages/AddServices/AddServices.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useContext } from "react";
+import { toast } from "react-toastify";
+import swal from "sweetalert";
+import { AuthContext } from "../../Context/AuthProvider";
 
 const AddServices = () => {
+  const { user } = useContext(AuthContext);
+
+  //used for add new comment or review.
+  const handleForm = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const title = form.title.value;
+    const img = form.imgURL.value;
+    const details = form.details.value;
+    const ratings = form.ratings.value;
+    const price = form.price.value;
+
+    if (ratings < 0 || ratings > 5) {
+      return swal("Please enter ratings between 0 to 5 numbers.");
+    } else {
+      //new service
+      const service = {
+        writerName: user.displayName,
+        email: user.email,
+        title: title,
+        img: img,
+        ratings: ratings,
+        price: price,
+        details: details,
+      };
+
+      fetch(`http://localhost:5000/services`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(service),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.acknowledged) {
+            toast.success("Your service has been successfully added.");
+            form.reset();
+          }
+        });
+    }
+  };
+
   return (
     <div>
       <h1 className="text-amber-400 text-3xl font-semibold my-8">
         Please fill up the below form to add your new service.
       </h1>
       <div className="border-2 rounded mx-4 my-8 p-5 shadow-lg">
-        <form>
+        <form onSubmit={handleForm}>
           <div className="grid sm:grid-cols-2 gap-4">
             <input
               className="border outline-none px-2 py-1 rounded"
