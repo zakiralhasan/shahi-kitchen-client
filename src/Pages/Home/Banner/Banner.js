50b896e126cb809b3aec25c5de2f8fd32acabb2c
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-10 11:54:10 +0600

    banner section design done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,9 +1,46 @@
 import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Autoplay, Navigation } from "swiper";
+import "swiper/css/navigation";
+import banner1 from "../../../images/banner/banner1.png";
+import banner2 from "../../../images/banner/banner2.png";
+import banner3 from "../../../images/banner/banner3.png";
 
 const Banner = () => {
   return (
-    <div>
-      <h1>Banner</h1>
+    <div className="w-11/12 mx-auto my-8">
+      <Swiper
+        modules={[Autoplay, Navigation]}
+        spaceBetween={30}
+        slidesPerView={1}
+        loop
+        autoplay
+        navigation
+        initialSlide={0}
+      >
+        <SwiperSlide>
+          <img
+            className=" h-58 border-8 border-gray-400 rounded-lg"
+            src={banner1}
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className=" h-58 border-8 border-gray-400 rounded-lg"
+            src={banner2}
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className=" h-58 border-8 border-gray-400 rounded-lg"
+            src={banner3}
+            alt=""
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -5,7 +5,6 @@ import Services from "../Services/Services";
 const Home = () => {
   return (
     <div>
-      <h1>Home page</h1>
       <Banner></Banner>
       <Services></Services>
     </div>
